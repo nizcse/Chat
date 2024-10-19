0bde2e7fa4e4f97539df728f6322acb8ac231928
nizcse <66297072+nizcse@users.noreply.github.com>
2024-10-20 01:22:29 +0530

    Feat!:"authToken validation implemented"

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -5,16 +5,27 @@ const users = []; // Temporary storage for user data (replace with DB in product
 
 // Signup logic
 exports.signup = async (req, res) => {
-    const { username, password } = req.body;
-    console.log(username, password, 'lol')
-    const hashedPassword = await bcrypt.hash(password, 10);
-    users.push({ username, password: hashedPassword });
-    res.status(201).send({ message: 'User created successfully' });
+    const { username, password } = req?.body;
+    if(username&&password){
+        console.log(username, password, 'lol')
+        const userExists = users.find(user => user.username === username);
+        if(userExists){
+            return res.status(400).send({ message: 'User already exists.' });
+        }else{
+            const hashedPassword = await bcrypt.hash(password, 10);
+            users.push({ username, password: hashedPassword });
+            res.status(201).send({ message: 'User created successfully' });
+        }
+    }
+    else{
+        return res.status(400).send({ message: 'Could not get credentials' });
+    }
 };
 
 // Login logic
 exports.login = async (req, res) => {
-    const { username, password } = req.body;
+    const { username, password } = req?.body;
+    if(username&&password){
     const user = users.find(user => user.username === username);
 
     if (!user) {
@@ -28,4 +39,24 @@ exports.login = async (req, res) => {
 
     const token = jwt.generateToken({ username: user.username });
     res.send({ message: 'Login successful', token });
+}
+else{
+    return res.status(400).send({ message: 'Could not get credentials' });
+}
 };
+
+exports.authToken = async (req, res) => {
+    const {authToken} = req?.body;
+    if(authToken){
+        try {
+            const decoded = jwt.verifyToken(authToken);
+            res.send({ message: 'Valid Token', user:decoded.username });
+          } catch (err) {
+            return res.status(400).send({ message: 'Invalid token' });
+          }
+    }
+    else{
+        return res.status(400).send({ message: 'Invalid or no token' });
+
+    }
+}
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signup, login } = require('../controller/authController');
+const { signup, login, authToken } = require('../controller/authController');
 const router = express.Router();
 
 // Signup route
@@ -7,5 +7,7 @@ router.post('/signup', signup);
 
 // Login route
 router.post('/login', login);
+
+router.post('/authToken', authToken)
 
 module.exports = router;
